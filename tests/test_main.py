13ab5ca91ab1a76b19f19384cb5c4678d5fbc2d6
lesyaolexs <34139337+lesyaolexs@users.noreply.github.com>
2022-05-27 01:22:37 +0300

    Add tests for limit and offset options

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,6 +7,10 @@ from app.main import app
 
 client = TestClient(app)
 
+path = Path(__file__).parent.resolve()
+with open(path / "mock_data" / "users_response.json") as file:
+    expected_response = json.load(file)
+
 
 def test_healthcheck():
     response = client.get("/")
@@ -17,11 +21,22 @@ def test_healthcheck():
 
 def test_get_users():
     response = client.get("/users")
+
     assert response.ok
-
-    path = Path(__file__).parent.resolve()
-
-    with open(path / "mock_data" / "users_response.json") as file:
-        expected_response = json.load(file)
-
     assert response.json() == expected_response
+
+
+def test_get_users_limit():
+    for i in range(0, 3):
+        response_limits_option = client.get(f"/users?limit={i}")
+
+        assert response_limits_option.ok
+        assert len(response_limits_option.json()) == i
+
+
+def test_get_users_offset():
+    for i in range(0, 3):
+        response_offset_option = client.get(f"/users?offset={i}")
+
+        assert response_offset_option.ok
+        assert response_offset_option.json() == expected_response[i:]
