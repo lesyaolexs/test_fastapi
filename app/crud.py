247913ad27fc5cf4664fb7083d43d512689e4310
lesyaolexs <34139337+lesyaolexs@users.noreply.github.com>
2022-05-28 02:09:45 +0300

    Add update_user endpoint

--- app/crud.py
+++ app/crud.py
@@ -5,7 +5,7 @@ import sqlalchemy
 
 from app.database import db
 from app.models.user import User as modelsUser
-from app.schemas.user import DBUser
+from app.schemas.user import DBUser, PatchUser
 from app.schemas.user import User as schemasUser
 
 
@@ -29,3 +29,16 @@ async def create_user(user: schemasUser) -> DBUser:
     )
     last_record_id = await db.execute(query)
     return DBUser.parse_obj({**user.dict(), "id": last_record_id})
+
+
+async def update_user(user_id: UUID, user: PatchUser) -> Optional[DBUser]:
+    query = (
+        sqlalchemy.update(modelsUser)
+        .where(modelsUser.id == user_id)
+        .values(
+            first_name=user.first_name, last_name=user.last_name, birthday=user.birthday
+        )
+    )
+    await db.execute(query)
+
+    return await get_user(user_id)
